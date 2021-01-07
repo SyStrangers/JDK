c3dbc972aa21ee94b5c3fb11b025ca205e3d959e
bishiyuan <3286978744@qq.com>
2021-01-07 09:35:49 +0800

    update StringBuffer

--- Jdk-learn/src/com/test/lang/StringBufferTest.java
+++ Jdk-learn/src/com/test/lang/StringBufferTest.java
@@ -1,10 +1,17 @@
 package com.test.lang;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringBufferTest {
 
-
-
     public static void main(String[] args) {
+
+        //底层实现为char
+        //默认的char数组长度为16
         StringBuffer stringBuffer = new StringBuffer();
+        //初始化长度为 字符串+16
+        StringBuffer str = new StringBuffer("test");
     }
+
 }
