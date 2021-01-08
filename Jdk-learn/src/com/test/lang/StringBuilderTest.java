da85c42111fc9f773aa97cea7d98b3e12cea1c32
bishiyuan <3286978744@qq.com>
2021-01-08 17:31:38 +0800

    update StringBuilder

---
+++ Jdk-learn/src/com/test/lang/StringBuilderTest.java
@@ -0,0 +1,10 @@
+package com.test.lang;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        //跟StringBuffer 同样都继承了 AbstractStringBuilder
+        //StringBuilder 线程不安全 但是比StringBuffer 快
+        //
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
