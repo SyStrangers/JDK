9c4b665be5939810465980ba5c71c6d0f5f0a1c5
bishiyuan <3286978744@qq.com>
2021-01-08 09:00:12 +0800

    update StringBuffer

--- Jdk-learn/src/com/test/lang/StringBufferTest.java
+++ Jdk-learn/src/com/test/lang/StringBufferTest.java
@@ -9,6 +9,8 @@ public class StringBufferTest {
 
         //底层实现为char
         //默认的char数组长度为16
+        // 继承于AbstractStringBuilder
+        //每次append 都会加上当前字符串的长度
         StringBuffer stringBuffer = new StringBuffer();
         //初始化长度为 字符串+16
         StringBuffer str = new StringBuffer("test");
