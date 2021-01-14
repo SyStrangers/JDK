f3b9bc6be873e0d0b06791b716b168094d008dec
bishiyuan <3286978744@qq.com>
2021-01-14 11:22:16 +0800

    add Class

--- Jdk-learn/src/com/test/util/AbstractListTest.java
+++ Jdk-learn/src/com/test/util/AbstractListTest.java
@@ -1,10 +1,15 @@
 package com.test.util;
 
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class AbstractListTest {
 
     public static void main(String[] args) {
-//        AbstractList list =
+//        AbstractList  继承这个类
+        //ArrayList 有个限制 数组长度不能超过 2的31次方-1
+        ArrayList temp = new ArrayList(2147483647);
+
     }
 }
