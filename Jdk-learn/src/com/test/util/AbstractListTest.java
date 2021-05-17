266e83e5d0f6027e2d00e0c2642e5a0ced4dfec3
bishiyuan <3286978744@qq.com>
2021-05-17 08:28:25 +0800

    update

--- Jdk-learn/src/com/test/util/AbstractListTest.java
+++ Jdk-learn/src/com/test/util/AbstractListTest.java
@@ -1,15 +1,23 @@
 package com.test.util;
 
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.Collections;
+import javax.print.attribute.IntegerSyntax;
+import java.util.*;
 
 public class AbstractListTest {
 
     public static void main(String[] args) {
 //        AbstractList  继承这个类
         //ArrayList 有个限制 数组长度不能超过 2的31次方-1
-        ArrayList temp = new ArrayList(2147483647);
+//        ArrayList temp = new ArrayList(2147483639);
 
+        Map<String,Integer> test = new HashMap<String,Integer>();
+        test.put("test1",12);
+        System.out.println(test);
+        test(test);
+
+    }
+
+    public static void test(Map<String, Integer> map){
+        map.put("test1",13);
     }
 }
